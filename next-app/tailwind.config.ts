09cb6f02c9b689bd2fabfae0185d9a72b94817e9
Jiale Shang <shang8024@gmail.com>
2025-05-07 02:56:44 -0400

    landing, map, house card carousel

---
+++ next-app/tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+ 
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        move: "move 5s linear infinite",
+      },
+      keyframes: {
+        move: {
+          "0%": { transform: "translateX(-200px)" },
+          "100%": { transform: "translateX(200px)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+ 
+export default config;
