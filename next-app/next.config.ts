178faf14a2fe80541a45f4bf582dd33d689264e1
Jiale Shang <shang8024@gmail.com>
2025-05-08 18:52:18 -0400

    Update next.config.ts

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx', 'server.ts'],
   distDir: "out",
